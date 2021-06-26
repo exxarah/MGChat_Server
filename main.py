a518844ae7815fa915488bed325cbae8257ab171
Exxarah <sarahbeavan@gmail.com>
2021-06-26 23:40:49 +1200

    Implement new messaging protocol
    This is significantly slower than the previous version, but uses a lot less data and is more extensible.
    More investigation is needed to determine why it performs so poorly

--- main.py
+++ main.py
@@ -15,25 +15,28 @@ players = {}
 
 def accept(sock, mask):
     conn, addr = sock.accept()
-    print("New connection recieved:", conn, addr)
+    print("New connection received:", conn, addr)
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read_packet)
 
     # register new item in players dictionary, but without any information about where they are yet until they send us it
-    players[conn.getpeername()] = {"con": conn, "info": False}
-    # send event letting people know I exist
-    # register myself to find out when new people join
+    players[conn.getpeername()] = {"con": conn, "registered": False, "net_id": None, "position": None, "partial_data": b""}
 
 def disconnect_client(conn):
-    players.pop(conn.getpeername())
-    print("Client disconnected:", conn)
-    sel.unregister(conn)
-    conn.close()
+    try:
+        players.pop(conn.getpeername())
+        print("Client disconnected:", conn)
+        sel.unregister(conn)
+        conn.close()
+    except OSError:
+        # TODO: We need to come back and fix this
+        pass
     return
 
 def read_packet(conn, mask):
+    peer = conn.getpeername()
     try:
-        data = conn.recv(1000)
+        data = conn.recv(1024)
     except (ConnectionResetError, ConnectionAbortedError, ConnectionError) as e:
         disconnect_client(conn)
         return
@@ -43,56 +46,112 @@ def read_packet(conn, mask):
         disconnect_client(conn)
         return
 
-    # We may have gotten more than one message from the client. For now, lets just take the latest one
-    # TODO: When we support more than just position updates, we may need to read every message, not just the latest
-    data = data.decode('utf-8')
-    data = data.split("\n")[-2]
+    # Append this message to the previous message
+    players[peer]["partial_data"] += data
 
+    if len(players[peer]["partial_data"]) > 10000:
+        print("Client sent too much data at one time!")
+        print(players[peer]["partial_data"])
+        disconnect_client(conn)
+        return
+
+    try:
+        while b"\n" in players[peer]["partial_data"]:
+            end_of_first_command = players[peer]["partial_data"].find(b"\n")
+            data = players[peer]["partial_data"][0:end_of_first_command]
+
+            # Lastly, before we parse it, we need to remove this part of the string from the partial data
+            players[peer]["partial_data"] = players[peer]["partial_data"][end_of_first_command+1:]
+
+            # We may have gotten more than one message from the client. For now, lets just take the latest one
+            # TODO: When we support more than just position updates, we may need to read every message, not just the latest
+            data = data.decode('utf-8')
+            parse_message_from_client(conn, data, peer)
+    except OSError:
+        print("Player disconnected while parsing messages")
+        disconnect_client(conn)
+        return
+
+def parse_message_from_client(conn, data, peer):
     # Parse the information as JSON and store it so we can serve it to clients later
     try:
-        d = json.loads(data)
+        client_command = json.loads(data)
     except:
-        print("Invalid data from client!")
+        print("Could not parse JSON sent by client")
         print(data)
         disconnect_client(conn)
         return
 
-    if len(d) != 1:
-        try:
-            players.pop(conn.getpeername())
-        except:
-            pass
-        print("Invalid data from client!")
-        print(d)
-        sel.unregister(conn)
-        conn.close()
+    # Check that the command type is valid
+    if client_command['$type'] == "MGChat.Commands.ServerConnectCommand, MGChat":
+        # TODO: Handle authentication
+        handle_registration(conn, client_command, peer)
         return
 
-    # TODO: Check if the player name is already in use -- no duplicates!!!!
-    if d[0] == players[conn.getpeername()]["info"]:
-        # A client sent us data but they haven't moved!
+    if client_command['$type'] == "MGChat.Commands.SetPositionCommand, MGChat":
+        handle_movement(conn, client_command, peer)
         return
 
-    # Update the players position and let every client know about it :)
-    players[conn.getpeername()]["info"] = d[0]
-    notify_clients()
+    print("Invalid command type received from client. Disconnecting them")
+    print(client_command)
+    disconnect_client(conn)
+
+def handle_registration(conn, server_connect_command, peer):
+    if players[peer]["registered"]:
+        print("Client attempted to register for a second time")
+        disconnect_client(conn)
+        return
+
+    # Validate they have a reasonable name
+    name = str(server_connect_command['NetId'])
+    if len(name) > 20:
+        print("Client attempted to connect with name that was too long!")
+        disconnect_client(conn)
+        return
+
+    # TODO: Validate it doesn't have special characters etc
+
+    players[peer]["registered"] = True
+    players[peer]["net_id"] = name
+
+    # Now, send the client a list of all player positions
+    handle_movement(conn, server_connect_command, peer)
+
+def handle_movement(conn, setPositionCommand, peer):
+    # Check they are registered before handling their movement
+    if not players[peer]["registered"]:
+        print("Client attempted to set a position before registration")
+        disconnect_client(conn)
+        return
+
+    # Check whether they have actually moved. If they haven't, we can bail out early!
+    if setPositionCommand['Position'] == players[peer]["position"]:
+        print("A client sent us a position, but they hadn't moved! Client bug?")
+        return
+
+    players[peer]["position"] = setPositionCommand['Position']
+
+    # We now need to let every client know that this person has moved position
+    # We can construct some raw JSON that pretends to be a real C# object
+    server_command = {
+        '$type': 'MGChat.Commands.SetRemotePositionCommand, MGChat',
+        'Position': setPositionCommand['Position'],
+        'NetId': players[peer]["net_id"]
+    }
+
+    notify_clients(conn, server_command)
 
 
-def notify_clients():
-    print("updating positions")
+def notify_clients(conn, server_command):
     for _, player in players.items():
-        # Send new information to the client about the position of all players (except the current player)
-        data_to_send = []
+        # Skip the current player (the one who sent us this command)
+        if player["con"] == conn:
+            continue
 
-        for _, value in players.items():
-            # Ignore self
-            if value["con"] == player["con"]:
-                continue
-            if value["info"]:
-                data_to_send.append(value["info"])
-
-        data = json.dumps(data_to_send).encode()
+        # Send the command
+        data = json.dumps(server_command).encode()
         player["con"].send(data + b"\n")
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -105,22 +164,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         for key, mask in events:
             callback = key.data
             callback(key.fileobj, mask)
-
-'''
-
-    conn, addr = s.accept()
-
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-
-            if not data:
-                break
-            if random.random() < 0.001:
-                print(data)
-            # dummy data!!!
-            i += 0.001
-            data = '[{"NetId":"ss23","Position":"' + str(int(i)) + ', 100"}]'
-            conn.sendall(data.encode('utf-8'))
-'''
